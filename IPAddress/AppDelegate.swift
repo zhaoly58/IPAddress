8dad83662ca0cd294f8aeeea56d6bfe79d669e4f
Jimmy Zhao <55856976+zhaoly58@users.noreply.github.com>
2025-06-22 20:59:11 +0900

    Apply uri scheme ("ipapp://")

--- IPAddress/AppDelegate.swift
+++ IPAddress/AppDelegate.swift
@@ -10,16 +10,24 @@ import FirebaseCore
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-      if let path = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") {
-          print("Firebase config found at: \(path)")
-      } else {
-          print("❌ Firebase config not found!")
-      }
-      
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        if let path = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") {
+            print("Firebase config found at: \(path)")
+        } else {
+            print("❌ Firebase config not found!")
+        }
+        
+        return true
+    }
+    
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        print("Received URL: \(url.absoluteString)")
+        return true
+    }
+    
 }

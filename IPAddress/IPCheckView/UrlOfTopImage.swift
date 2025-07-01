6f2ba4b0a6d9c952fe7f71e2d79909b768928891
Jimmy Zhao <jimmyzly88@gmail.com>
2025-07-01 23:28:37 +0900

    Update HP image url

--- IPAddress/IPCheckView/UrlOfTopImage.swift
+++ IPAddress/IPCheckView/UrlOfTopImage.swift
@@ -8,4 +8,6 @@
 import Foundation
 
 //let urlOfTopImage : String = "https://www.google.co.jp/logos/doodles/2024/mothers-day-2024-may-12-6753651837110364-l.png"
-let urlOfTopImage : String = "https://picsum.photos/536/354"
+//let urlOfTopImage : String = "https://picsum.photos/536/354"
+
+let urlOfTopImage : String = "https://wallpapercat.com/w/full/0/f/3/5815630-3840x2160-desktop-hd-4k-wallpaper-image.jpg"

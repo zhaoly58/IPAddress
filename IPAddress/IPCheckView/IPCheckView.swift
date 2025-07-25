275239c9c40725114d099ae578f0a81842c61821
Jimmy Zhao <jimmyzly88@gmail.com>
2025-07-25 22:45:55 +0900

    Update: redesign HP button UI

--- IPAddress/IPCheckView/IPCheckView.swift
+++ IPAddress/IPCheckView/IPCheckView.swift
@@ -53,33 +53,36 @@ struct IPCheckTab: View {
                     .padding(.horizontal)
                 
                 VStack{
-                    Button("Check") {
-                        fetchDataFromAPI()
-                        isTextFieldFocused = false
-                    }
-                    .frame(width: UIScreen.main.bounds.width - 320, height: 30)
-                    .background(.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.blue, lineWidth: 2)
-                    }
-                    .alert(isPresented: $isShowingAlert) {
-                        Alert(title: Text("报错了!"), message: Text("请输入 IP 地址"), dismissButton: .default(Text("欧克~")))
+                    HStack {
+                        Button("Clear") {
+                            userInputIP = ""
+                        }
+                        .frame(width: UIScreen.main.bounds.width - 320, height: 30)
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.red, lineWidth: 2)
+                        )
+                        
+                        Button("Check") {
+                            fetchDataFromAPI()
+                            isTextFieldFocused = false
+                        }
+                        .frame(width: UIScreen.main.bounds.width - 320, height: 30)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.blue, lineWidth: 2)
+                        }
+                        .alert(isPresented: $isShowingAlert) {
+                            Alert(title: Text("报错了!"), message: Text("请输入 IP 地址"), dismissButton: .default(Text("欧克~")))
+                        }
                     }
                     
-                    Button("Clear") {
-                        userInputIP = ""
-                    }
-                    .frame(width: UIScreen.main.bounds.width - 320, height: 30)
-                    .background(Color.red)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.red, lineWidth: 2)
-                    )
                     
                     
                     Button("My IP Address"){
